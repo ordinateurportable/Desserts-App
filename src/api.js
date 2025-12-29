651eb30791a2c0943878489b81f7a97ae49fceff
Stepan Makarov <makarow282@gmail.com>
2025-12-29 12:25:19 +0300

    final build

--- src/api.js
+++ src/api.js
@@ -1,99 +1,7 @@
-// const apiURL = "http://localhost:3000"
-
-// export async function getProducts() {
-//         const resp = await fetch(`${apiURL}/products`); 
-//         if(!resp.ok) {
-//             throw new Error("Ошибка получения товаров")
-//         }
-//         const data = await resp.json();
-//         return data;
-// } 
-
-// export async function getProductByID(id) {
-//     const resp = await fetch(`${apiURL}/products/${id}`)
-//     if(!resp.ok) {
-//         return null
-//     }
-//     const data = await resp.json()
-//     return data
-// }
-
-// export async function getProductFromCartByID(id) {
-//     const resp = await fetch(`${apiURL}/cart/${id}`)
-
-//     if(!resp.ok) {
-//         return null
-//     } else {
-//         const data = await resp.json()
-//         return data
-//     }
-// }
-
-// async function addProductToCartFirst(id)  {
-//     const resp = await fetch(`${apiURL}/cart`, {
-//     method: "POST",
-//     "Content-Type": "application/json", 
-//     body: JSON.stringify({id, count: 1})
-//     })
-
-//     if(!resp.ok) {
-//         throw new Error("Ошибка добавления товара")
-//     }
-//  }
-
-//  async function updateCartProductCount(id, count) {
-//     const resp = await fetch (`${apiURL}/cart/${id}`, {
-//         method: "PATCH", 
-//         "Content-type": "application/json",
-//         body: JSON.stringify({count}),
-//     })
-
-//     if(!resp.ok) {
-//         throw new Error("Ошибка выполнения запроса")
-//     }
-//  }
-
-//  export async function addProductToCart(id) {
-//     const data = await getProductByID(id)   
-
-//     const productInCart = await getProductFromCartByID(id)
-
-//     if(!productInCart) {
-//         await addProductToCartFirst(id)
-//     } else {
-//         const count = productInCart.count
-//         await updateCartProductCount(id, count + 1)
-//         return {...data, count: count + 1}
-//     }
-// }
-
-// export async function removeProductFromCart(id) {
-//     const product = await getProductFromCartByID(id)
-//     if(product.count === 1) {
-//         const resp = await fetch(`${apiURL}/cart/${id}`, {
-//             method: "DELETE"
-//         })
-
-//         if(!resp.ok) {
-//             throw new Error("Ошибка удаления товара")
-//         }
-
-//     } else if (product.count > 1) {
-//         await updateCartProductCount(id, product.count - 1)
-//     }
-    
-// }
-
 const apiUrl = "http://localhost:3000";
 
-export async function getProducts() {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 500);
-  });
-
-  const resp = await fetch(`${apiUrl}/products`);
+export async function getProducts(uri = "products") {
+  const resp = await fetch(`${apiUrl}/${uri}`);
   if (!resp.ok) {
     throw new Error("Ошибка получения товаров");
   }
@@ -147,7 +55,6 @@ async function updateCartProductCount(id, count) {
   }
 }
 
-//REFACTOR
 export async function addProductToCart(id) {
   const data = await getProductById(id);
 
@@ -164,7 +71,11 @@ export async function addProductToCart(id) {
 }
 
 export async function removeProductFromCart(id) {
-  const product = await getProductFromCartById(id);
+    const [product, { price }] = await Promise.all([
+    getProductFromCartById(id),
+    getProductById(id),
+  ]).then((data) => data);
+
   if (product.count === 1) {
     const resp = await fetch(`${apiUrl}/cart/${id}`, {
       method: "DELETE",
@@ -175,5 +86,62 @@ export async function removeProductFromCart(id) {
     }
   } else if (product.count > 1) {
     await updateCartProductCount(id, product.count - 1);
+  }
+
+  return {
+    count: product.count - 1,
+    price,
+  };
+
+}
+
+export async function makeOrder() {
+  const cartResp = await fetch(`${apiUrl}/cart`);
+  if (!cartResp.ok) {
+    throw new Error("Ошибка оформления заказа");
+  }
+
+  const cart = await cartResp.json();
+
+  // Получаем текущий orderId
+  const getOrderIdResp = await fetch(`${apiUrl}/orderId`);
+  if (!getOrderIdResp.ok) {
+    throw new Error("Ошибка оформления заказа");
+  }
+
+  const orderIdArr = await getOrderIdResp.json();
+  const newOrderId = orderIdArr[0].orderId + 1;
+  console.log(newOrderId);
+
+  for (const product of cart) {
+    const ordersResp = await fetch(`${apiUrl}/orders`, {
+      method: "POST",
+      "Content-Type": "application/json",
+      body: JSON.stringify({ ...product, orderId: newOrderId }),
+    });
+
+    if (!ordersResp.ok) {
+      throw new Error("Ошибка оформления заказа2");
+    }
+  }
+
+  for (const product of cart) {
+    const deleteResp = await fetch(`${apiUrl}/cart/${product.id}`, {
+      method: "DELETE",
+    });
+    if (!deleteResp.ok) {
+      throw new Error("Ошибка оформления заказа");
+    }
+  }
+
+  //Обновление orderID
+  const orderUpdateResp = await fetch(`${apiUrl}/orderId/1`, {
+    method: "PUT",
+    "Content-Type": "application/json",
+    body: JSON.stringify({ orderId: newOrderId }),
+  });
+
+  if (!orderUpdateResp.ok) {
+    throw new Error("Ошибка оформления заказа");
   }
 }
--- src/cart.js
+++ src/cart.js
@@ -1,81 +1,25 @@
-// import { addProductToCart, removeProductFromCart } from "./api"
-// import { printError } from "./error"
-// import { spinner } from "./spinner"
-
-// let cartCount = 0
-// let orderTotal = 0
-
-// function cartCalcCount() {
-//     cartCount++
-//     // document.querySelector(".cart-count").textContent = Number(document.querySelector(".cart-count").textContent) + 1
-//     document.querySelector(".cart-count").textContent = cartCount
-// } 
-
-// function calcOrderTotal(productPrice) {
-//     orderTotal += productPrice
-//     document.querySelector(".total-price").textContent = orderTotal.toFixed(2)
-// }
-
-// document.querySelector(".product-list").addEventListener("click", async (e) => {
-//     if(e.target.matches(".add-to-cart, .add-to-cart *")) {
-//         const id = e.target.closest(".product").dataset.id
-//         const btn = e.target.closest(".add-to-cart")
-//         try {
-//             btn.disabled = true;
-//             spinner(`.product[data-id="${id}"]`, "afterbegin", 30)
-//             const product = await addProductToCart(id)
-//             if (product.count === 1) {
-//                 const productMarkup = `
-//                 <li class="cart-item" data-id="${product.id}">
-//                     <span>${product.name}</span>
-//                     <span>x<span class="count">${product.count}</span></span>
-//                     <span>$${product.price}</span>
-//                     <img class="remove-item" src="assets/icons/icon-remove-item.svg" alt="">
-//                 </li>
-//                 `
-
-//                 document 
-//                     .querySelector(".cart-items")
-//                     .insertAdjacentHTML("beforeend", productMarkup)
-            
-//             } else if (product.count > 1) {
-//                 document.querySelector(`.cart-item[data-id="${id}"] .count`).textContent = product.count
-//             }
-
-//             calcOrderTotal(product.price)
-
-//         } catch (error) {
-//             printError(error)
-//         } finally {
-//             cartCalcCount()
-//             document.querySelector(".confirm-order").disabled = false
-//             btn.disabled = false
-//             document.querySelector("#spinner").remove()
-//         }
-//     }
-// })
-
-// document.querySelector(".cart-items").addEventListener("click", async (e) => {
-//     if(e.target.matches(".remove-item")) {
-//         const id = e.target.closest(".cart-item").dataset.id 
-//         await removeProductFromCart(id)
-//     }
-// })
-
-import { addProductToCart, getProductById, removeProductFromCart } from "./api";
+import { addProductToCart, getProductById, getProducts, removeProductFromCart, makeOrder } from "./api";
 import { printError } from "./error";
 import { spinner } from "./spinner";
 
 let cartCount = 0;
 let orderTotal = 0;
 
-function calcCartCount() {
-  cartCount++;
-  document.querySelector(".cart-count").textContent = cartCount;
+function calcCartCount(isAdd = true) {
+    if (isAdd) {
+    cartCount++;
+    } else {
+      cartCount--;
+    }
+    document.querySelector(".cart-count").textContent = cartCount;
 }
 
-function calcOrderTotal(productPrice) {
-  orderTotal += productPrice;
+function calcOrderTotal(productPrice, isAdd = true) {
+  if (isAdd) {
+    orderTotal += productPrice;
+  } else {
+    orderTotal -= productPrice;
+  }
   document.querySelector(".total-price").textContent = orderTotal.toFixed(2);
 }
 
@@ -121,8 +65,103 @@ document.querySelector(".product-list").addEventListener("click", async (e) => {
 document.querySelector(".cart-items").addEventListener("click", async (e) => {
   if (e.target.matches(".remove-item")) {
     const id = e.target.closest(".cart-item").dataset.id;
-    await removeProductFromCart(id);
+    try {
+      const { count: productCount, price } = await removeProductFromCart(id);
+      if(productCount === 0) {
+        e.target.closest(".cart-item").remove()
+      } else {
+        e.target.closest(".cart-item").querySelector(".count").textContent = productCount 
+      }
+      calcCartCount(false);
+      if (cartCount === 0) {
+        document.querySelector(".confirm-order").disabled = true;
+      }
+      calcOrderTotal(price, false);
+
+    } catch (error) {
+      printError(error)
+    }
+
   }
 });
 
-// document.addEventListener("DOMContentLoaded", () => {});
+async function loadProductsFromCart() {
+  const cartProducts = await getProducts("cart")
+  for (let i = 0; i < cartProducts.length; i++) {
+    const product = await getProductById(cartProducts[i].id);
+    cartProducts[i].name = product.name;
+    cartProducts[i].price = product.price;
+  }
+
+  return cartProducts;
+}
+
+function renderCartProducts(cartProducts) {
+  const markup = cartProducts.map(
+    (product) => `
+          <li class="cart-item" data-id="${product.id}">
+            <span>${product.name}</span>
+            <span>x<span class="count">${product.count}</span></span>
+            <span>$${product.price}</span>
+            <img class="remove-item" src="assets/icons/icon-remove-item.svg" alt="">
+          </li>
+          `
+  );
+
+  document
+    .querySelector(".cart-items")
+    .insertAdjacentHTML("beforeend", markup.join(""));
+}
+
+function renderTotals(cartProducts) {
+  const data = cartProducts.reduce(
+    (acc, product) => ({
+      totalCount: acc.totalCount + product.count,
+      totalPrice: acc.totalPrice + product.count * product.price,
+    }),
+    { totalCount: 0, totalPrice: 0 }
+  );
+
+  cartCount = data.totalCount;
+  orderTotal = Number(data.totalPrice.toFixed(2));
+
+  document.querySelector(".cart-count").textContent = cartCount;
+  document.querySelector(".total-price").textContent = orderTotal;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+    const cartProducts = await loadProductsFromCart();
+    //отрисовать товары
+    renderCartProducts(cartProducts);
+    //пересчитать count, price
+    renderTotals(cartProducts);
+    //раздизейбл кнопки
+    if (cartCount > 0) {
+      document.querySelector(".confirm-order").disabled = false;
+    }
+    //оформить заказ
+  } catch (error) {
+    printError({ message: "Ошибка получения товаров из корзины" });
+  }
+});
+
+document.querySelector(".confirm-order").addEventListener("click", async () => {
+  try {
+    const cartProducts = await loadProductsFromCart();
+    const output = cartProducts.map(
+      (product, ind) =>
+        `${ind + 1}. ${product.name} х${product.count} | $${product.price}\n`
+    );
+    const msg = `Ваш заказ:\n\n${output.join("")}\n\nПодтвердите ваш заказ`;
+    if (!confirm(msg)) {
+      return;
+    } else {
+      await makeOrder();
+      alert("Ваш заказ уже в пути ✅");
+      location.reload();
+    }
+  } catch (error) {
+    printError(error);
+  }
+});
